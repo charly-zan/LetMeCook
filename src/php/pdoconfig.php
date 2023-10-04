8886385307415a088b31853599da87740719650e
carlitos <carlos9720@live.com.mx>
2023-10-04 09:57:57 -0600

    yes

--- src/php/pdoconfig.php
+++ src/php/pdoconfig.php
@@ -1,6 +1,6 @@
 <?php
     $servername = "localhost";
-    $database = "letmecook";
-    $username = "root";
-    $password = "";
+    $database = "id21349947_letmecook";
+    $username = "id21349947_root";
+    $password = "Carlos9720*";
 ?>
